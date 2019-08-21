ab785ce0d7808e75a1bd20d4cb8efd095c15c697
Fabricio <fabricioobedin@gmail.com>
2019-08-20 22:34:37 -0300

    using cors

--- app.js
+++ app.js
@@ -23,18 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With. Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
